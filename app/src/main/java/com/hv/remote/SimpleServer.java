2e90b33a07df9d0ccb7ca11e4424f44445d9f010
54wall <weiqiang_1989@126.com>
2018-06-01 11:30:45 +0800

    init 从ecplise导入到as报图片错误还有报AAPT2 error，删除.9位图即可。

---
+++ app/src/main/java/com/hv/remote/SimpleServer.java
@@ -0,0 +1,129 @@
+package com.hv.remote;
+
+import android.util.Log;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public abstract class SimpleServer implements Runnable {
+    public final String TAG = this.getClass().getSimpleName();
+    private final CopyOnWriteArrayList<ConnectionThread> mConnectionThreads = new CopyOnWriteArrayList<ConnectionThread>();
+    private Callback mCallback = null;
+
+    private ServerSocket mServer;
+    private volatile boolean mStopServer = true;
+    private Thread mServerThread;
+
+    protected abstract void handleConnection(Socket socket) throws IOException;
+
+    public void run() {
+        Log.v(TAG, "Server is running");
+
+        while (!mStopServer) {
+            try {
+                Socket socket = mServer.accept();
+
+                Log.v(TAG, "Accept socket: " + socket.getPort());
+                if (!mStopServer) {
+                    startConnectionThread(socket);
+                } else {
+                    socket.close();
+                }
+            } catch (IOException e) {
+                if (!mStopServer) {
+                    Log.e(TAG, e.getMessage());
+                }
+            }
+        }
+    }
+
+    public void start(int port) throws IOException {
+        mServer = new ServerSocket(port);
+
+        mStopServer = false;
+        mServerThread = new Thread(this);
+        mServerThread.start();
+    }
+
+    public void close() {
+        mStopServer = true;
+        if (mServer != null) {
+            try {
+                mServer.close();
+            } catch (IOException e) {
+                Log.e(TAG, e.getMessage());
+            }
+        }
+
+        for (ConnectionThread connectionThread : mConnectionThreads) {
+            connectionThread.close();
+        }
+
+        mCallback = null;
+    }
+
+    public void setCallback(Callback callback) {
+        mCallback = callback;
+    }
+
+    public int getNumberOfConnections() {
+        return mConnectionThreads.size();
+    }
+
+    private void startConnectionThread(final Socket socket) {
+        ConnectionThread connectionThread = new ConnectionThread(socket);
+        mConnectionThreads.add(connectionThread);
+
+        connectionThread.start();// ������������
+    }
+
+    public interface Callback {
+        public void onConnect();
+
+        public void onDisconnect();
+    }
+
+    private final class ConnectionThread extends Thread {
+        private final Socket mmSocket;
+
+        private ConnectionThread(Socket socket) {
+            setName("SimpleServer ConnectionThread" + getId());
+            mmSocket = socket;
+        }
+
+        @Override
+        public void run() {
+            Log.v(TAG, "Server thread " + getId() + " started");
+
+            if (mCallback != null) {
+                mCallback.onConnect();
+            }
+
+            try {
+                handleConnection(mmSocket);
+            } catch (IOException e) {
+                if (!mStopServer) {
+                    Log.e(TAG, e.getMessage());
+                }
+            }
+
+            if (mCallback != null) {
+                mCallback.onDisconnect();
+            }
+
+            Log.v(TAG, "Server thread " + getId() + " died");
+        }
+
+        private void close() {
+            if (mmSocket != null) {
+                try {
+                    mmSocket.close();
+                } catch (IOException e) {
+                    Log.e(TAG, e.getMessage());
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/hv/remote/ui/AboutActivity.java
@@ -0,0 +1,17 @@
+package com.hv.remote.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.hv.remote.R;
+
+public class AboutActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+
+}
